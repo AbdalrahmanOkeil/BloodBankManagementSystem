526440343ee98e9812376bb686e75a766b28a967
Abd Alrahman Salah <abdalrahmanokeil@gmail.com>
2026-01-11 16:12:48 +0200

    Initial commit - BloodBank project

--- BloodBank.Infrastructure/Identity/UserService.cs
+++ BloodBank.Infrastructure/Identity/UserService.cs
@@ -1,20 +1,31 @@
 ﻿using BloodBank.Application.DTOs;
 using BloodBank.Application.Interfaces.Services;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 
 namespace BloodBank.Infrastructure.Identity
 {
     public class UserService : IUserService
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        public UserService(UserManager<ApplicationUser> userManager)
+        private readonly ILogger<UserService> _logger;
+        public UserService(UserManager<ApplicationUser> userManager, ILogger<UserService> logger)
         {
             _userManager = userManager;
+            _logger = logger;
         }
         public async Task<UserDTO?> GetUserByIdAsync(string userId)
         {
+            _logger.LogInformation("Start fetching user with ID {userId}", userId);
+
             var user = await _userManager.FindByIdAsync(userId);
-            if (user == null) return null;
+            if (user == null)
+            {
+                _logger.LogWarning("User with ID {userId} not found", userId);
+                return null;
+            }
+
+            _logger.LogInformation("User with ID {userId} found: {email}", userId, user.Email);
 
             return new UserDTO
             {
